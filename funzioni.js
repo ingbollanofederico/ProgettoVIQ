55080a86518663bff598b7eb504c12a48c6d98aa
Federico Bollano <s234201@studenti.polito.it>
2019-05-30 18:27:46 +0200

    Merge branch 'master' of https://github.com/ingbollanofederico/ProgettoVIQ
    # Conflicts:
    #	stile.css
    #	tesina.html

--- funzioni.js
+++ funzioni.js
@@ -1,3 +1,13 @@
+function visibilityStelle(x){
+
+    if(x!="TotaleHotel")
+
+        document.f.sceltaStelle.style.visibility="hidden";
+
+    else
+        document.f.sceltaStelle.style.visibility="visible";
+}
+
 function loadASync(url, success) {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
@@ -79,12 +89,12 @@ window.onload = function () {
         originalTabRegioni.append(tableElement);
     });
 
-    Plotly.d3.csv("DATASET_REGIONI.csv", function (error, data) {
+    Plotly.d3.csv("DATASET_REGIONI_MOD.csv", function (error, data) {
             //Creazione
             let regioni = [];
             let numHotel = [];
 
-            for (let i = 1; i < data.length; i++) {
+            for (let i = 0; i < data.length; i++) {
                 let regione = data[i]["Regione"];
                 regioni.push(regione);
 
@@ -119,16 +129,119 @@ window.onload = function () {
         });
 };
 
-function visibilityStelle(x){
 
-    if(x!="Hotel")
+function grafico(f1){
 
-        document.f.sceltaStelle.style.visibility="hidden";
 
-    else
-        document.f.sceltaStelle.style.visibility="visible";
+    let regione = f1.sceltaLuogo.value; //italia vs regioni
+    let struttura = f1.sceltaStrutture.value; //hotel beb altri
+
+    if(struttura==="Hotel"){
+        let stelle = f1.sceltaStelle.value; //stelle attive se hotel
+    }
+
+    let turisti = f1.turisti.checked; //checked turisti
+
+
+    if(regione==="Regione") {
+
+        Plotly.d3.csv("DATASET_REGIONI_MOD.csv", function (error, data) {
+            //Creazione
+            let regioni = [];
+            let numStruttura = [];
+
+            if(turisti) {
+                var numTuristi = [];
+            }
+
+
+            for (let i = 0; i < data.length; i++) {
+                let reg = data[i][regione];
+                regioni.push(reg);
+
+
+                let strut = data[i][struttura];
+                numStruttura.push(strut);
+
+                if(turisti) {
+                    let tur = data[i]["Presenze2017"];
+                    numTuristi.push(tur);
+                }
+
+            }
+            console.log("R: "+regioni+" S: "+numStruttura+" T: "+numTuristi);
+
+
+             trace1 = [{
+                name: "regioni",
+                y: numStruttura,
+                x: regioni,
+                orientation: "v",
+                type: "bar",
+
+                marker: {color: 'salmon'},
+            }];
+
+            if(turisti) {
+                trace2 = [{
+                    name: "regioni",
+                    y: numTuristi,
+                    x: regioni,
+                    orientation: "v",
+                    type: "bar",
+
+                    marker: {color: 'blue'},
+                }];
+            }
+
+            if(turisti){
+                var layout1 = {
+
+                    margin: {t: 40, l: 70, r: 30, b: 100},
+
+                    barmode: "group",
+
+                    hovermode: "y",
+
+                    legend: {
+                        y: 1.02, x: 0.5,
+                        yanchor: "bottom",
+                        xanchor: "center",
+                        orientation: "h",
+                    },
+
+                }
+            }else{
+                    var layout2 = {
+
+                        margin: {t: 40, l: 70, r: 30, b: 100},
+
+                        hovermode: "y",
+
+                        legend: {
+                            y: 1.02, x: 0.5,
+                            yanchor: "bottom",
+                            xanchor: "center",
+                            orientation: "h",
+                        },
+                }
+            }
+
+            if(turisti){
+                let data = [trace1,trace2];
+                Plotly.plot("graph", data, layout1);
+            }else{
+                Plotly.plot("graph", trace1, layout2);
+            }
+        });
+    }
+    /*else{
+
+        DA FARE IL CASO DELLE PROVINCE UNA AD UNA
+
+    }*/
+
 }
-
 
 
 
