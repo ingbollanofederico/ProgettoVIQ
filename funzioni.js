c8e173b0d2ccaa340ce9f04ca4be729922526c56
Alessandro Baldo <s236651@studenti.polito.it>
2019-06-03 18:04:00 +0200

    Merge branch 'master' of https://github.com/ingbollanofederico/ProgettoVIQ
    # Conflicts:
    #	tesina.html

--- funzioni.js
+++ funzioni.js
@@ -1,3 +1,21 @@
+
+function colorLink(x){
+    let element=document.getElementById(x);
+    element.style.color="rgba(150,0,0,1)";
+
+
+}
+
+function decolorLink(x){
+    let element=document.getElementById(x);
+    element.style.color="azure";
+
+
+}
+
+
+
+
 function visibilityStelle(x){
 
     if(x!="TotaleHotel")
@@ -88,8 +106,92 @@ window.onload = function () {
         let originalTabRegioni = document.getElementById('originalTabRegioni');
         originalTabRegioni.append(tableElement);
     });
+    //grafico default introduzione
+    Plotly.d3.csv("DATASET_REGIONI_MOD.csv", function (error, data) {
 
-    //grafico default 1
+
+        //Creazione
+        let regioni = [];
+        let numHotel = [];
+        let numBeb=[];
+        let numExtra=[];
+
+        for (let i = 0; i < data.length; i++) {
+            let regione = data[i]["Regione"];
+            regioni.push(regione);
+
+            let hotel = data[i]["TotaleHotel"];
+            numHotel.push(hotel);
+
+            let beb=data[i]["TotaleBeB"];
+            numBeb.push(beb);
+
+            let extra=data[i]["TotaleAltreStrutture"];
+            numExtra.push(extra);
+        }
+        console.log("A:" + regioni + " B:" + numHotel);
+
+        trace1 = {
+            name: "Hotel",
+            y: numHotel,
+            x: regioni,
+            /*text:numHotel.map(String),
+            textposition: 'outside',
+            */
+            hoverinfo:'x+y',
+            orientation: "v",
+            type: "bar",
+
+            marker: {color: 'salmon', opacity: 0.6},
+        };
+        trace2 = {
+            name: "B&B",
+            y: numBeb,
+            x: regioni,
+            /*text:numBeb.map(String),
+            textposition: 'outside',*/
+            hoverinfo:'x+y',
+            orientation: "v",
+            type: "bar",
+
+            marker: {color: 'blue', opacity: 0.6},
+        };
+        trace3 = {
+            name: "Strutture Extra-Alberghiere",
+            y: numExtra,
+            x: regioni,
+            /*text:numExtra.map(String),
+            textposition: 'outside',*/
+            hoverinfo:'x+y',
+
+
+            orientation: "v",
+            type: "bar",
+
+            marker: {color: 'gree', opacity: 0.6},
+        };
+        let layout = {
+
+            margin: {t: 40, l: 70, r: 30, b: 100},
+
+            hovermode: "x+y",
+
+            legend: {
+                y: 1.02, x: 0.5,
+                yanchor: "bottom",
+                xanchor: "center",
+                orientation: "h",
+
+            },
+
+
+        };
+        data=[trace1, trace2, trace3];
+
+        Plotly.plot("graphIntroduttivo", data, layout);
+    });
+
+    //grafico default pagina grafici
     Plotly.d3.csv("DATASET_REGIONI_MOD.csv", function (error, data) {
 
         let s="TutteStrutture";
@@ -173,9 +275,10 @@ window.onload = function () {
             data=[trace1, trace2, trace3];
 
             Plotly.plot("graph", data, layout);
+
         });
 
-    //grafico default 2
+    //grafico default 2 pagina grafici
     Plotly.d3.csv("DATASET_REGIONI_MOD.csv", function (error, data) {
         //Creazione
         let regioni = [];
